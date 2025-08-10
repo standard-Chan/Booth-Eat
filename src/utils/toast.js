027817453f7c6af6656d5774fc9e28d6a5bac5ed
standard-chan <starp321@naver.com>
2025-08-10 18:22:41 +0900

    feat: toastify util 에 구현

---
+++ src/utils/toast.js
@@ -0,0 +1,25 @@
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// 공통 토스트 함수
+export const showSuccessToast = (message) => {
+  toast.success(message, {
+    position: 'top-center',
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
+};
+
+export const showErrorToast = (message) => {
+  toast.error(message, {
+    position: 'top-center',
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
+};
