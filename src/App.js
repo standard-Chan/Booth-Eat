5b2b2332755ead4485f981c87ee4862f64eb6d9c
standard-chan <starp321@naver.com>
2025-08-13 02:10:44 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import { useEffect } from 'react';
 import './App.css';
 import AppToast from './components/common/AppToast.jsx';
 import AppRouter from './routes/approuter.jsx';
 
 function App() {
-  useEffect(() => {
-    // console.log(`process.env.OPENAI_API_KEY : ${(process.env.OPENAI_API_KEY) ?? ""}`);
-    // console.log(`process.env.local.OPENAI_API_KEY : ${(process.env.local.OPENAI_API_KEY) ?? ""}`);
-    console.log(`process.env.REACT_APP_OPENAI_API_KEY : ${process.env.REACT_APP_OPENAI_API_KEY}`);
-    // console.log(`VITE_OPENAI_API_KEY : ${process.env.VITE_OPENAI_API_KEY}`);
-  })
   return (<>
     <AppToast />
     <AppRouter />
